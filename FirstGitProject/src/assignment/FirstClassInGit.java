b81cb7c25111c088ab69e5ee681365bcc182afb9
cm11774 <cm11774@CEIT2553214X031.ad.georgiasouthern.edu>
2019-08-21 16:50:12 -0400

    Asked user for their last name

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,4 +1,5 @@
 package assignment;
+import java.util.Scanner;
 
 public class FirstClassInGit 
 {
@@ -6,6 +7,11 @@ public class FirstClassInGit
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello, my name is Chris!");
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("What's your last name?");
+		String userInput = input.nextLine();
+		System.out.println(userInput + " is a nice name!");
 		
 		//add a statement to display your name in the console
 		
