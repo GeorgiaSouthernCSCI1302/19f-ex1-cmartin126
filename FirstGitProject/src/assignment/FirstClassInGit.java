cfcdda18d39d355b3beea6ce0f3efef554ec6d47
cm11774 <cm11774@CEIT2553214X031.ad.georgiasouthern.edu>
2019-08-21 16:34:26 -0400

    Changed so that text now displays a greeting from me, Chris.

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,9 +1,11 @@
 package assignment;
 
-public class FirstClassInGit {
+public class FirstClassInGit 
+{
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello, my name is Chris!");
 		
 		//add a statement to display your name in the console
 		
